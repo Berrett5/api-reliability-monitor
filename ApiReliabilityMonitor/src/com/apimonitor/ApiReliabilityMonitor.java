fa8e2bea3e0c3be77dfaf9052d53d3942438698a
Berrett5 <bradigan00005@yahoo.com>
2026-03-14 18:52:37 -0600

    Create MonitorRepository

--- ApiReliabilityMonitor/src/com/apimonitor/ApiReliabilityMonitor.java
+++ ApiReliabilityMonitor/src/com/apimonitor/ApiReliabilityMonitor.java
@@ -4,18 +4,31 @@
  */
 package com.apimonitor;
 
-import com.apimonitor.database.DatabaseConnection;
+import com.apimonitor.database.MonitorRepository;
+import com.apimonitor.models.Monitor;
+import java.util.List;
 
 /**
  *
  * @author bradi
  */
+
 public class ApiReliabilityMonitor {
     
     public static void main(String[] args){
         
         System.out.println("Starting API Reliability Monitor...");
-        DatabaseConnection.getConnection();
+
+        MonitorRepository repository = new MonitorRepository();
+        List<Monitor> monitors = repository.getAllMonitors();
+
+        for (Monitor monitor : monitors) {
+            System.out.println("Monitor ID: " + monitor.getMonitorId());
+            System.out.println("Name: " + monitor.getName());
+            System.out.println("URL: " + monitor.getUrl());
+            System.out.println("HTTP Method: " + monitor.getHttpMethod());
+            System.out.println("----------------------------");
+        }
         
     }
 }
